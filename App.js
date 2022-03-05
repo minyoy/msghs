e4cb6e58ebc17209372dc459d1eddb35e0a7a2ed
minyoy <65756020+minyoy@users.noreply.github.com>
2022-03-05 21:56:33 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -1,30 +1,130 @@
-import React, { useState } from "react";
+import './App.css';
+import logo from "./img/logo.PNG"
+import fork from "./img/fork.png"
+import React, {useState} from 'react';
 import axios from "axios";
 
 function App() {
-  const findbreak="breakfast"
-  const findlun="lunch"
-  const finddin="dinner"
-  const findgwal="]"
-  const [data, setData] = useState(null);
 
-  const MonClick = () => {
-  axios
-    .get('https://schoolmenukr.ml/api/high/Q100000247?month=12&date=27')
-    .then((response) => {
-    setData(response.data);
-    });
-  };
-  return(
-    <div>
+    const[data, setData] = useState(null);
+    var menu = new Array();
+
+    const MonClick = async() => {
+    try{
+      const response = await axios.get('https://schoolmenukr.ml/api/high/Q100000247?year=2022&month=3&date=7&allergy=hidden');
+      setData(response.data);
+      var menulist = response.data.menu[0];
+      var breakfast = menulist['breakfast']
+      var lunch = menulist['lunch']
+      var dinner = menulist['dinner']
+    }
+    catch(e) {
+    console.log(e);
+    }
+    }
+
+    const TueClick = async() => {
+      try{
+        const response = await axios.get('https://schoolmenukr.ml/api/high/Q100000247?month=3&date=8&allergy=hidden');
+        setData(response.data);
+        var menulist = response.data.menu[0];
+        var breakfast = menulist['breakfast']
+        var lunch = menulist['lunch']
+        var dinner = menulist['dinner']
+      }
+      catch(e) {
+      console.log(e);
+      }
+    }
+
+    const WedClick = async() => {
+      try{
+        const response = await axios.get('https://schoolmenukr.ml/api/high/Q100000247?month=3&date=9&allergy=hidden');
+        setData(response.data);
+        var menulist = response.data.menu[0];
+        var breakfast = menulist['breakfast']
+        var lunch = menulist['lunch']
+        var dinner = menulist['dinner']
+      }
+      catch(e) {
+      console.log(e);
+      }
+    }
+
+    const ThuClick = async() => {
+      try{
+        const response = await axios.get('https://schoolmenukr.ml/api/high/Q100000247?month=3&date=10&allergy=hidden');
+        setData(response.data);
+        var menulist = response.data.menu[0];
+        var breakfast = menulist['breakfast']
+        var lunch = menulist['lunch']
+        var dinner = menulist['dinner']
+      }
+      catch(e) {
+      console.log(e);
+      }
+    }
+
+    const FriClick = async() => {
+      try{
+        const response = await axios.get('https://schoolmenukr.ml/api/high/Q100000247?month=3&date=11&allergy=hidden');
+        setData(response.data);
+        var menulist = response.data.menu[0];
+        var breakfast = menulist['breakfast']
+        var lunch = menulist['lunch']
+        var dinner = menulist['dinner']
+      }
+      catch(e) {
+      console.log(e);
+      }
+    }
+    
+  return (    
+    <div className="App">
       <div>
-        <button MonClick={MonClick}>Monday</button>
+        <img className='logo' src={logo} alt="logo"></img>
       </div>
-      {JSON.stringify(data, null, 2)}
-      {document.writeln(data.indexOf(findbreak))}
+      <div id = 'flex-container1'>
+        <button onClick={MonClick} className='week'>Mon</button>
+        <button onClick={TueClick} className='week'>Tue</button>
+        <button onClick={WedClick} className='week'>Wed</button>
+        <button onClick={ThuClick} className='week'>Thu</button>
+        <button onClick={FriClick} className='week'>Fri</button>
+      </div>
+      <div className='flex-container2'>
+        <div className='box'>
+          <div className='flex-container3'>
+            <img className='fork' src={fork} alt=""></img>
+            <div class="today">오늘의 조식</div>
+          </div>
+          <div className='content'>
+            {data && data.menu[0]['breakfast'].length != 0 && <textarea value={JSON.stringify(data.menu[0]['breakfast'], null, 2)} readOnly={true} />}
+            {data && data.menu[0]['breakfast'].length == 0 && <textarea>급식이 없습니다</textarea>}
+          </div>
+        </div>
+        <div className='box'>
+          <div className='flex-container3'>
+            <img className='fork' src={fork} alt=""></img>
+            <div class="today">오늘의 중식</div>
+          </div>
+          <div className='content'>
+            {data && data.menu[0]['lunch'].length != 0 && <textarea value={JSON.stringify(data.menu[0]['lunch'], null, 2)} readOnly={true} />}
+            {data && data.menu[0]['lunch'].length == 0 && <textarea>급식이 없습니다</textarea>}
+          </div>
+        </div>
+        <div className='box'>
+          <div className='flex-container3'>
+            <img className='fork' src={fork} alt=""></img>
+            <div class="today">오늘의 석식</div>
+          </div>
+          <div className='content'> 
+            {data && data.menu[0]['dinner'].length != 0 && <textarea value={JSON.stringify(data.menu[0]['dinner'], null, 2)} readOnly={true} />}
+            {data && data.menu[0]['dinner'].length == 0 && <textarea>급식이 없습니다</textarea>}
+          </div>
+        </div>
+      </div>
     </div>
-  );
-        
+  )
 }
 
 export default App;
